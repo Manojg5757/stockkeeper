89fcba8c9131c1ea768dd379162b3e42a4eeeb74
Manojg5757 <gmanojece2021@gmail.com>
2026-03-13 11:52:41 +0400

    Use environment variables for Firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDesaNZdtnHyRfPJy4vfNaOaUfJ21cVOEo",
-  authDomain: "stockkeeper-a02a7.firebaseapp.com",
-  projectId: "stockkeeper-a02a7",
-  storageBucket: "stockkeeper-a02a7.firebasestorage.app",
-  messagingSenderId: "27264752036",
-  appId: "1:27264752036:web:c9526b3b53fbd4b1290931"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
